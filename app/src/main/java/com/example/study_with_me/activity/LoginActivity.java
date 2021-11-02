a70b41f49b355a831425400a4413180493100ae6
jinuk <flwu123@naver.com>
2021-11-02 22:57:48 +0900

    로그인, 회원가입, 마이페이지, 스터디 등록

---
+++ app/src/main/java/com/example/study_with_me/activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.study_with_me.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.study_with_me.R;
+import com.example.study_with_me.SignUpActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        TextView Textview;
+        Textview = findViewById(R.id.textView6);
+
+        Textview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
