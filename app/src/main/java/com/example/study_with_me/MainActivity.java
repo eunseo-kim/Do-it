cab063f7d81bb748ea296a13c0f452f13ea415ff
tar9et <flwu123@naver.com>
2021-11-03 00:15:47 +0900

    test

--- app/src/main/java/com/example/study_with_me/MainActivity.java
+++ app/src/main/java/com/example/study_with_me/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                 startActivity(intent);
+
             }
         });
 
