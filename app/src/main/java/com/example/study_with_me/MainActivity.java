120b4af2c0c836ad7ac08ff93806040f6d5f251f
jinuk <flwu123@naver.com>
2021-11-02 22:29:36 +0900

    a

--- app/src/main/java/com/example/study_with_me/MainActivity.java
+++ app/src/main/java/com/example/study_with_me/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.study_with_me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+
+        TextView Textview;
+        Textview = findViewById(R.id.textView6);
+
+        Textview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
