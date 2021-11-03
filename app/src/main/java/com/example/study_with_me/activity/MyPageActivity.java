6e8b4cf033beb7564c89d604ed6f58ae542af264
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-11-04 06:39:37 +0900

    feat: MyPageActivity.java 추가

---
+++ app/src/main/java/com/example/study_with_me/activity/MyPageActivity.java
@@ -0,0 +1,15 @@
+package com.example.study_with_me.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.study_with_me.R;
+
+public class MyPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
