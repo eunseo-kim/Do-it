ad2a7801c45e6c15eda288e716ac3c66ad9e14cf
KodaHye <kodhdh09@gmail.com>
2021-11-05 23:55:51 +0900

    commit

---
+++ app/src/main/java/com/example/study_with_me/model/Studydata.java
@@ -0,0 +1,22 @@
+package com.example.study_with_me.model;
+
+public class Studydata {
+    private String recuit;
+    private String field;
+    private String title;
+    private String date;
+
+    public Studydata(String recuit, String field, String title, String date) {
+        this.recuit = recuit;
+        this.field = field;
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getStudyRecuit() {return this.recuit;}
+    public String getStudyField() {return this.field;}
+    public String getStudyTitle() {return this.title;}
+    public String getStudydate() {return this.date;}
+
+
+}
