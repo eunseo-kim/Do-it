1cb916b952e08b7a57cfdb44e89999232872a6c6
KodaHye <kodhdh09@gmail.com>
2021-11-05 11:08:08 +0900

    commit

--- app/src/main/java/com/example/study_with_me/activity/StudySearchActivity.java
+++ app/src/main/java/com/example/study_with_me/activity/StudySearchActivity.java
@@ -11,18 +11,31 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.study_with_me.R;
+import com.example.study_with_me.adapter.StudyAdpter;
+import com.example.study_with_me.model.Studydata;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class StudySearchActivity extends AppCompatActivity {
+    ArrayList<Studydata> studyDataList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.study_search_main);
+
+        this.InitializeStudyData();
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        final StudyAdpter study= new StudyAdpter(this, studyDataList);
+        listView.setAdapter(study);
 
         // 상단 메뉴바
         getSupportActionBar().setTitle("스터디 검색");
@@ -84,5 +97,13 @@ public class StudySearchActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    public void InitializeStudyData() {
+        studyDataList = new ArrayList<Studydata>();
+        studyDataList.add(new Studydata("모집중", "취업", "모바일 프로그래밍 성실한 스터디 그룹원 구합니다.", "오늘 04:12"));
+        studyDataList.add(new Studydata("모집중", "취업", "청주 공기업 NCS스터디 모집합니다.", "2021.10.17"));
+        studyDataList.add(new Studydata("모집중", "프로그래밍", "코테준비하실분", "2021.10.15"));
+        studyDataList.add(new Studydata("모집중", "어학", "한기대 TOEIC스터디 하실 분!", "2021.10.13"));
+        studyDataList.add(new Studydata("모짐중", "프로그래밍", "딥러닝 스터디 구해요~", "2021.10.15"));
     }
 }
--- app/src/main/java/com/example/study_with_me/adapter/StudyAdpter.java
+++ app/src/main/java/com/example/study_with_me/adapter/StudyAdpter.java
@@ -1,2 +1,67 @@
-package com.example.study_with_me.adapter;public class StudyAdpter {
+package com.example.study_with_me.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.study_with_me.R;
+import com.example.study_with_me.activity.StudySearchActivity;
+import com.example.study_with_me.model.Studydata;
+
+import java.util.ArrayList;
+
+public class StudyAdpter extends BaseAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+    ArrayList<Studydata> studyList;
+
+    public StudyAdpter(Context context, ArrayList<Studydata> studyList) {
+        this.context = context;
+        this.studyList = studyList;
+        this.layoutInflater = LayoutInflater.from(this.context);
+    }
+
+    @Override
+    public int getCount() {
+        return studyList.size();
+    }
+//
+//    @Override
+//    public Object getItem(int i) {
+//        return i;
+//    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    public Studydata getItem(int position) {return studyList.get(position);}
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        View view = layoutInflater.inflate(R.layout.study_search_item , null);
+
+        TextView recuit = (TextView)view.findViewById(R.id.studyRecuit);
+        TextView field = (TextView)view.findViewById(R.id.studyField);
+        TextView title = (TextView)view.findViewById(R.id.studyTitle);
+        TextView date = (TextView)view.findViewById(R.id.studyRegisterDay);
+
+
+//        LinearLayout studyArea = (LinearLayout)convertView.findViewById(R.id.);
+//        studyArea.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//            }
+//        });
+
+        recuit.setText(studyList.get(i).getStudyRecuit());
+        field.setText(studyList.get(i).getStudyField());
+        title.setText(studyList.get(i).getStudyTitle());
+        date.setText(studyList.get(i).getStudydate());
+        return view;
+    }
 }
