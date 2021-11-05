5824b52211936cdd9a7c42c6533e2ffee39334c1
23Yong <66981851+23Yong@users.noreply.github.com>
2021-11-05 14:09:20 +0900

    complete creation of StudyGroup data

--- app/src/main/java/com/example/study_with_me/model/StudyGroup.java
+++ app/src/main/java/com/example/study_with_me/model/StudyGroup.java
@@ -4,12 +4,24 @@ import java.util.Date;
 
 public class StudyGroup {
     private String name;
+    private String description;
     private String type;
     private int numOfMember;
-    private Date term;
+    private Date startDate;
+    private Date endDate;
+
+    public StudyGroup(String name, String description, String type, int numOfMember, Date startDate, Date endDate) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.numOfMember = numOfMember;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 
     private String getName() { return name; }
+    private String getDescription() { return description; }
     public String getType() { return type; }
     public int getMember() { return numOfMember; }
-    public Date getTerm() { return term; }
+
 }
