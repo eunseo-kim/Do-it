edf611538a509e9c7654c10ca013493062d14aa4
KodaHye <kodhdh09@gmail.com>
2021-11-06 12:54:10 +0900

    edit ui

--- app/src/main/java/com/example/study_with_me/activity/StudySearchActivity.java
+++ app/src/main/java/com/example/study_with_me/activity/StudySearchActivity.java
@@ -36,7 +36,7 @@ public class StudySearchActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
-        adapter.addItem("모집중", "취업", "모바일 프로그래밍 성실한 스터디 그룹원 구합니다.", "오늘 04:12");
+        adapter.addItem("모집중", "프로그래밍", "모바일 프로그래밍 성실한 스터디 그룹원 구합니다.", "오늘 04:12");
         adapter.addItem("모집중", "취업", "청주 공기업 NCS스터디 모집합니다.", "2021.10.17");
         adapter.addItem("모집중", "프로그래밍", "코테준비하실분", "2021.10.15");
         adapter.addItem("모집중", "어학", "한기대 TOEIC스터디 하실 분!", "2021.10.13");
