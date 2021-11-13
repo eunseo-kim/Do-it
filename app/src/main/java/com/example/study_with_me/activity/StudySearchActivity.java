6a09a5aba48d5331c7c7e38ee7f3b90ded9f75f7
KodaHye <kodhdh09@gmail.com>
2021-11-14 02:16:12 +0900

    filtering people and period

--- app/src/main/java/com/example/study_with_me/activity/StudySearchActivity.java
+++ app/src/main/java/com/example/study_with_me/activity/StudySearchActivity.java
@@ -51,7 +51,7 @@ public class StudySearchActivity extends AppCompatActivity {
 
         studySearchListView = (ListView) findViewById(R.id.studySearchListView);
 
-        if(firebaseAuth.getCurrentUser() != null){
+        if (firebaseAuth.getCurrentUser() != null) {
             userID = firebaseAuth.getCurrentUser().getUid();
         }
         // 상단 메뉴바
@@ -60,24 +60,25 @@ public class StudySearchActivity extends AppCompatActivity {
 
         /** floatingActionButton 누르면 스터디 생성 화면 **/
         floatingButtonClickedListener();
-        
+
         /** 펼치기 버튼(expandButton) 클릭 시 필터링 검색 창 펼침 **/
         expandButtonClickedListener();
 
         /** DB의 studygroups의 변화가 생겼을 때 감지하는 listener **/
         setStudyGroupsChangedListener();
 
-        EditText editText = (EditText)findViewById(R.id.editText) ;
+        EditText editText = (EditText) findViewById(R.id.editText);
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void afterTextChanged(Editable edit) {
                 String filterText = edit.toString();
-                if(filterText.length() > 0) {
+                if (filterText.length() > 0) {
                     studySearchListView.setFilterText(filterText);
                 } else {
                     studySearchListView.clearTextFilter();
                 }
             }
+
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -88,7 +89,10 @@ public class StudySearchActivity extends AppCompatActivity {
             }
         });
     }
-    /** 액션바 오버라이딩 **/
+
+    /**
+     * 액션바 오버라이딩
+     **/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -96,7 +100,9 @@ public class StudySearchActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    /** 스터디 검색화면 리스트 뷰 처리 **/
+    /**
+     * 스터디 검색화면 리스트 뷰 처리
+     **/
     private void setListView(ArrayList<Map<String, StudyGroup>> list) {
         SearchAdapter adapter = new SearchAdapter(this, list);
         studySearchListView.setAdapter(adapter);
@@ -111,27 +117,31 @@ public class StudySearchActivity extends AppCompatActivity {
         });
     }
 
-    /** DB에 있는 모든 스터디 정보 가져오는 함수 **/
+    /**
+     * DB에 있는 모든 스터디 정보 가져오는 함수
+     **/
     private void collectAllStudyGroups(Map<String, Object> studygroups) {
-        for(Map.Entry<String, Object> entry : studygroups.entrySet()) {
+        for (Map.Entry<String, Object> entry : studygroups.entrySet()) {
             Map singleStudyGroup = (Map) entry.getValue();
             studyList.add(singleStudyGroup);
         }
     }
 
-    /** DB의 studygroups에 무언가 추가되거나 변경될 시 처리하는 함수 **/
+    /**
+     * DB의 studygroups에 무언가 추가되거나 변경될 시 처리하는 함수
+     **/
     private void setStudyGroupsChangedListener() {
         studyGroupRef.addListenerForSingleValueEvent(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.getValue() != null) {
+                        if (snapshot.getValue() != null) {
                             collectAllStudyGroups((Map<String, Object>) snapshot.getValue());
                             setListView(studyList);
                             filteredStudyList = (ArrayList<Map<String, StudyGroup>>) studyList.clone();
 
                             // 스터디 목록 없으면 addMessage("스터디를 등록해주세요")출력하도록
-                            if(studyList.size() != 0) {
+                            if (studyList.size() != 0) {
                                 findViewById(R.id.addMessage).setVisibility(View.INVISIBLE);
                             }
                         }
@@ -144,22 +154,26 @@ public class StudySearchActivity extends AppCompatActivity {
         );
     }
 
-    /** floatingButton 처리 함수 **/
+    /**
+     * floatingButton 처리 함수
+     **/
     private void floatingButtonClickedListener() {
-                FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(R.id.floatingActionButton);
-                floatingActionButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), StudyRegisterActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-    /** 펼치기 버튼 처리 함수 **/
+    /**
+     * 펼치기 버튼 처리 함수
+     **/
     private void expandButtonClickedListener() {
-        ImageView expandButton = (ImageView)findViewById(R.id.expandButton);
-        LinearLayout filteringScreen = (LinearLayout)findViewById(R.id.filteringScreen);
+        ImageView expandButton = (ImageView) findViewById(R.id.expandButton);
+        LinearLayout filteringScreen = (LinearLayout) findViewById(R.id.filteringScreen);
         expandButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -171,10 +185,12 @@ public class StudySearchActivity extends AppCompatActivity {
             }
         });
     }
-    
-    /** 상단 바 마이페이지, 알림 버튼 **/
+
+    /**
+     * 상단 바 마이페이지, 알림 버튼
+     **/
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.alarmBell:
                 Intent intent1 = new Intent(this, AlarmActivity.class);
                 startActivity(intent1);
@@ -188,19 +204,22 @@ public class StudySearchActivity extends AppCompatActivity {
         }
     }
 
-    /** 분류 필터링 **/
+    /**
+     * 분류 필터링
+     **/
     public void onClick(View v) {
         filteredStudyList.clear();
 
         switch (v.getId()) {
             case R.id.all:
-                Toast.makeText(getApplicationContext(),"전체 스터디", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "전체 스터디", Toast.LENGTH_SHORT).show();
                 filteredStudyList = (ArrayList<Map<String, StudyGroup>>) studyList.clone();
-                break;
+
+
             case R.id.programming:
                 Toast.makeText(getApplicationContext(), "프로그래밍만 분류", Toast.LENGTH_SHORT).show();
                 for (Map<String, StudyGroup> sg : studyList) {
-                    if(String.valueOf(sg.get("type")).equals("프로그래밍")) {
+                    if (String.valueOf(sg.get("type")).equals("프로그래밍")) {
                         filteredStudyList.add(sg);
                     }
                 }
@@ -208,7 +227,7 @@ public class StudySearchActivity extends AppCompatActivity {
             case R.id.employ:
                 Toast.makeText(getApplicationContext(), "취업만 분류", Toast.LENGTH_SHORT).show();
                 for (Map<String, StudyGroup> sg : studyList) {
-                    if(String.valueOf(sg.get("type")).equals("취업")) {
+                    if (String.valueOf(sg.get("type")).equals("취업")) {
                         filteredStudyList.add(sg);
                     }
                 }
@@ -216,7 +235,7 @@ public class StudySearchActivity extends AppCompatActivity {
             case R.id.language:
                 Toast.makeText(getApplicationContext(), "어학 분류", Toast.LENGTH_SHORT).show();
                 for (Map<String, StudyGroup> sg : studyList) {
-                    if(String.valueOf(sg.get("type")).equals("어학")) {
+                    if (String.valueOf(sg.get("type")).equals("어학")) {
                         filteredStudyList.add(sg);
                     }
                 }
@@ -224,13 +243,14 @@ public class StudySearchActivity extends AppCompatActivity {
             case R.id.ect:
                 Toast.makeText(getApplicationContext(), "기타만 분류", Toast.LENGTH_SHORT).show();
                 for (Map<String, StudyGroup> sg : studyList) {
-                    if(String.valueOf(sg.get("type")).equals("기타")) {
+                    if (String.valueOf(sg.get("type")).equals("기타")) {
                         filteredStudyList.add(sg);
                     }
                 }
                 break;
         }
         setListView(filteredStudyList);
+
     }
 
     public void studyAreaClicked(View view) {
@@ -239,30 +259,138 @@ public class StudySearchActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /**인원수 필터링**/
+    /**
+     * 인원수 필터링
+     **/
     public void filterCount(View view) {
         ArrayList<Map<String, StudyGroup>> filterCountList = new ArrayList<Map<String, StudyGroup>>();
         switch (view.getId()) {
             case R.id.two:
                 for (Map<String, StudyGroup> sg : filteredStudyList) {
-                    if(Integer.valueOf(String.valueOf(sg.get("member"))) == 2) {
+                    if (Integer.valueOf(String.valueOf(sg.get("member"))) == 2) {
+                        filterCountList.add(sg);
+
+
+                    }
+
+                }
+                setListView(filterCountList);
+                break;
+            case R.id.three:
+                for (Map<String, StudyGroup> sg : filteredStudyList) {
+                    if (Integer.valueOf(String.valueOf(sg.get("member"))) == 3) {
                         filterCountList.add(sg);
                     }
                 }
                 setListView(filterCountList);
                 break;
-            case R.id.three:
-                break;
             case R.id.four:
+                for (Map<String, StudyGroup> sg : filteredStudyList) {
+                    if (Integer.valueOf(String.valueOf(sg.get("member"))) == 4) {
+                        filterCountList.add(sg);
+                    }
+                }
+                setListView(filterCountList);
                 break;
             case R.id.moreFour:
+                for (Map<String, StudyGroup> sg : filteredStudyList) {
+                    if (Integer.valueOf(String.valueOf(sg.get("member"))) > 4) {
+                        filterCountList.add(sg);
+                    }
+                }
+                setListView(filterCountList);
                 break;
         }
     }
 
     /**스터디 기간 필터링**/
     public void filerDate(View view) {
+        String startDate;
+        String endDate;
+        int month;
+        ArrayList<Map<String, StudyGroup>> filterCountList = new ArrayList<Map<String, StudyGroup>>();
+        switch(view.getId()) {
+            case R.id.oneMonth:
+                for(Map<String, StudyGroup> sg: filteredStudyList) {
+                    startDate = String.valueOf(sg.get("startDate"));
+                    endDate = String.valueOf(sg.get("endDate"));
 
+                    if(Integer.parseInt(endDate.substring(2,4)) > Integer.parseInt(startDate.substring(2,4))) {
+                        month = 12 - Integer.parseInt(startDate.substring(5,7)) + Integer.parseInt(endDate.substring(5,7));
+                        if(month == 1) {
+                            Log.d("기간", Integer.toString(month));
+                            filterCountList.add(sg);
+                        }
+                    } else {
+                        month = Integer.parseInt(endDate.substring(5,7)) - Integer.parseInt(startDate.substring(5,7));
+                        if(month == 1) {
+                            filterCountList.add(sg);
+                        }
+                    }
+                }
+                setListView(filterCountList);
+                break;
+            case R.id.twoMonth:
+                for(Map<String, StudyGroup> sg: filteredStudyList) {
+                    startDate = String.valueOf(sg.get("startDate"));
+                    endDate = String.valueOf(sg.get("endDate"));
+
+                    if(Integer.parseInt(endDate.substring(2,4)) > Integer.parseInt(startDate.substring(2,4))) {
+                        month = 12 - Integer.parseInt(startDate.substring(5,7)) + Integer.parseInt(endDate.substring(5,7));
+                        if(month == 2) {
+                            Log.d("기간", Integer.toString(month));
+                            filterCountList.add(sg);
+                        }
+                    } else {
+                        month = Integer.parseInt(endDate.substring(5,7)) - Integer.parseInt(startDate.substring(5,7));
+                        if(month == 2) {
+                            filterCountList.add(sg);
+                        }
+                    }
+                }
+                setListView(filterCountList);
+                break;
+            case R.id.sixMonth:
+                for(Map<String, StudyGroup> sg: filteredStudyList) {
+                    startDate = String.valueOf(sg.get("startDate"));
+                    endDate = String.valueOf(sg.get("endDate"));
+
+                    if(Integer.parseInt(endDate.substring(2,4)) > Integer.parseInt(startDate.substring(2,4))) {
+                        month = 12 - Integer.parseInt(startDate.substring(5,7)) + Integer.parseInt(endDate.substring(5,7));
+                        if(month == 6) {
+                            Log.d("기간", Integer.toString(month));
+                            filterCountList.add(sg);
+                        }
+                    } else {
+                        month = Integer.parseInt(endDate.substring(5,7)) - Integer.parseInt(startDate.substring(5,7));
+                        if(month == 6) {
+                            filterCountList.add(sg);
+                        }
+                    }
+                }
+                setListView(filterCountList);
+                break;
+            case R.id.moreSixMonth:
+                for(Map<String, StudyGroup> sg: filteredStudyList) {
+                    startDate = String.valueOf(sg.get("startDate"));
+                    endDate = String.valueOf(sg.get("endDate"));
+
+                    if(Integer.parseInt(endDate.substring(2,4)) > Integer.parseInt(startDate.substring(2,4))) {
+                        month = 12 - Integer.parseInt(startDate.substring(5,7)) + Integer.parseInt(endDate.substring(5,7));
+                        if(month > 6) {
+                            Log.d("기간", Integer.toString(month));
+                            filterCountList.add(sg);
+                        }
+                    } else {
+                        month = Integer.parseInt(endDate.substring(5,7)) - Integer.parseInt(startDate.substring(5,7));
+                        if(month > 6) {
+                            filterCountList.add(sg);
+                        }
+                    }
+                }
+                setListView(filterCountList);
+                break;
+        }
     }
 
 }
