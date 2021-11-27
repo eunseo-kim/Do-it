7d54e38de2f19506fdf789b6919a83b705c68b62
KodaHye <kodhdh09@gmail.com>
2021-11-28 02:59:09 +0900

    Edit UserInfo: Show userNickname

--- app/src/main/java/com/example/study_with_me/activity/UserInfo.java
+++ app/src/main/java/com/example/study_with_me/activity/UserInfo.java
@@ -1,9 +1,10 @@
 package com.example.study_with_me.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.study_with_me.R;
 
 public class UserInfo extends AppCompatActivity {
+
+    private TextView nameTextView;
     TextView btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,14 @@ public class UserInfo extends AppCompatActivity {
         getSupportActionBar().setTitle("사용자정보");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Intent intent= getIntent();
+
+        Log.d("받은이름", intent.getStringExtra("닉네임"));
+        String nickName = intent.getStringExtra("닉네임");
+        Log.d("닉네임", nickName);
+
+        nameTextView = (TextView) findViewById(R.id.userName);
+        nameTextView.setText(nickName);
         btn1 = (TextView) findViewById(R.id.close_button);
         btn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
--- app/src/main/java/com/example/study_with_me/adapter/ApplicantAdapter.java
+++ app/src/main/java/com/example/study_with_me/adapter/ApplicantAdapter.java
@@ -8,13 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
 import com.example.study_with_me.R;
 import com.example.study_with_me.activity.UserInfo;
 import com.example.study_with_me.model.Applicant;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.ArrayList;
 
 public class ApplicantAdapter extends BaseAdapter {
@@ -25,7 +23,7 @@ public class ApplicantAdapter extends BaseAdapter {
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
     private DatabaseReference UserRef = databaseReference.child("users");
-    private String username;
+    private String userName;
 
     public ApplicantAdapter(Context context, ArrayList<Applicant> applicants) {
         this.context = context;
@@ -50,19 +48,22 @@ public class ApplicantAdapter extends BaseAdapter {
         TextView alarmMemberName = (TextView) view.findViewById(R.id.alarmMemberName);
         TextView alarmRegisterTime = (TextView) view.findViewById(R.id.alarmRegisterTime);
 
-        Log.d("Applicant >>> ", applicants.toString());
-        Log.d("getUserName()", "??" + applicants.get(position).getUserName());
-        Log.d("getStudyGroupTitle()", ">" + applicants.get(position).getStudyGroupTitle());
+//        Log.d("Applicant >>> ", applicants.toString());
+//        Log.d("getUserName()", "??" + applicants.get(position).getUserName());
+//        Log.d("getStudyGroupTitle()", ">" + applicants.get(position).getStudyGroupTitle());
 
         studyTitle.setText(applicants.get(position).getStudyGroupTitle());
         alarmMemberName.setText(applicants.get(position).getUserName());
         alarmRegisterTime.setText(applicants.get(position).getRegisterTime());
 
+        String userName = alarmMemberName.getText().toString();
+//        Log.d("이름", userName);
         alarmMemberName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Toast.makeText(context, "사용자 정보 보여주자구!!!!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(context, UserInfo.class);
+                intent.putExtra("닉네임", userName);
 
                 context.startActivity(intent);
             }
