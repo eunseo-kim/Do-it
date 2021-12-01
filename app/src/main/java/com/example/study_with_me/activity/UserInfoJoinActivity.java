bd1ffc8e445f9a92019949de03fbe08270e778ad
KodaHye <kodhdh09@gmail.com>
2021-12-01 21:43:55 +0900

    feat: Show User Join ACtivity

--- app/src/main/java/com/example/study_with_me/activity/UserInfoJoinActivity.java
+++ app/src/main/java/com/example/study_with_me/activity/UserInfoJoinActivity.java
@@ -10,14 +10,25 @@ import android.widget.RadioGroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.study_with_me.R;
+import com.example.study_with_me.adapter.StudyGroupAdapter;
+
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class UserInfoJoinActivity extends AppCompatActivity {
+    private ArrayList<Map<String, Object>> filteredList = new ArrayList<>();
+    private StudyGroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_info_join_list);
         getSupportActionBar().setTitle("참여 스터디 목록");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        myStudyRoomListView = (Listview)findViewById()
     }
 
 
@@ -48,16 +59,29 @@ public class UserInfoJoinActivity extends AppCompatActivity {
     }
 
     public void setRadoiClicked() {
-//        RadioGroup userStudyRadioGroup = (RadioGroup) findViewById(R.id.userStudyRadioGroup);
-//        switch (userStudyRadioGroup.getCheckedRadioButtonId()) {
-//            case R.id.started:
+        RadioGroup userStudyRadioGroup = (RadioGroup) findViewById(R.id.userStudyRadioGroup);
+        switch (userStudyRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.started:
 //                filterStarted();
-//                break;
-//            case R.id.finished:
-//                filterFinished();
-//                break;
-//        }
+                break;
+            case R.id.finished:
+                filterFinished();
+                break;
+        }
+    }
+
+    public void setListView(ArrayList<Map<String, Object>> studyGroupList) throws ParseException {
+        adapter = new StudyGroupAdapter(this, studyGroupList);
+        userStudyRoomListView.
     }
 
     // 진행중 스터디 그룹 필터링
+
+    public void filterFinished() throws ParseException {
+        filteredList.clear();
+        for() {
+
+        }
+        setListView(filteredList);
+    }
 }
